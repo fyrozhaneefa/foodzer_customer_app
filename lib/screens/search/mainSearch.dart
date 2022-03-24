4bbbc5f227402d6367f7c9a313f88a1849c92b94
jjithin <jithinjithvr20012gamil.com>
2022-03-24 20:24:34 +0530

    menucommit

--- lib/screens/search/mainSearch.dart
+++ lib/screens/search/mainSearch.dart
@@ -310,6 +310,7 @@ class _MainSearchScreenState extends State<MainSearchScreen> {
                                       ],
                                     ),
                                   )
+
                                 ],
                               ),
                             );
