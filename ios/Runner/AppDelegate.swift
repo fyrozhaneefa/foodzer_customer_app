76d38006dc779aa694eb12e0a5f9f361f4db636a
riyas <rizbava6@gmail.com>
2022-07-11 22:47:51 +0530

    ios icon changed added ios firebase file new

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+    GMSServices.provideAPIKey("AIzaSyCzJV498CgnaMfwp1CdVkl6INwAy_ekPQI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
